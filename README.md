e16d037688765e84a23f803f3e66f1871b477894
ruchirajha <ruchirajha@gmail.com>
2019-07-15 21:33:02 +0000

    This is my first commit I am aadding a README file

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is a readme file
+
+Here i'm going to describe what is going on in my project
+
+And now we are going to try and get this onto my github account
+
+# All git commands are run in the terminal not the console
+
+git config --global user.email "ruchirajha@gmail.com"
+git config --global user.name "ruchirajha"
---
+++ rscript_rmj_github_practice.R
@@ -0,0 +1,11 @@
+# gitignore is a function that lists what it will always ignore and never put it on the internet
+
+.Rproj.user
+.Rhistory
+.RData
+.Ruserdata
+
+# this is the default of what is ignored
+
+# create a new textfile called "README.md"  from file--> new--> text file
+# go to the G+ sign sideways, and go to dropdown menu and commit
